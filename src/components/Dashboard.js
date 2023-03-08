f2b4b5a3cb0f8ba53364b53cdad92b0ff75572f2
Rama Ganapathy <ramaganapathy2222@gmail.com>
2023-03-08 10:38:14 +0000

    feat(dashboard): action card boiler plate

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,4 @@
+import { AddIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
@@ -5,127 +6,147 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  Flex,
   Heading,
   SimpleGrid,
   Text,
+  Tag,
+  TagLeftIcon,
+  TagLabel,
+  Icon,
+  useToast,
 } from "@chakra-ui/react";
 import React from "react";
-import { MdReceipt, MdOutlineInventory, MdPeopleAlt } from "react-icons/md";
+import {
+  MdReceipt,
+  MdOutlineInventory,
+  MdPeopleAlt,
+  MdRemoveRedEye,
+  MdAdd,
+  MdEMobiledata,
+} from "react-icons/md";
 
-export const DashBoard = () => (
-  <>
-    <Box p="2">
-      <Button
-        leftIcon={<MdReceipt size={"20px"} />}
-        variant="outline"
-        color="teal"
-        size={"sm"}
-      >
-        <Heading size="md">Invoice</Heading>
-      </Button>
-    </Box>
+export const DashBoard = () => {
+  const toast = useToast();
+  const callToast = () => {
+    toast({
+      title: "Account created.",
+      description: "We've created your account for you.",
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+      position: "top-right",
+    });
+  };
+  return (
+    <>
+      <Box p="2">
+        <Tag size="lg" colorScheme="teal" borderRadius="full">
+          <TagLeftIcon boxSize="20px" as={MdReceipt} />
+          <TagLabel>Invoice</TagLabel>
+        </Tag>
+      </Box>
 
-    <SimpleGrid
-      spacing={4}
-      templateColumns="repeat(auto-fill, minmax(20rem, 1fr))"
-    >
-      <Card>
-        <CardHeader>
-          <Heading size="md"> Customer dashboard</Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>View a summary of all your customers over the last month.</Text>
-        </CardBody>
-        <CardFooter>
-          <Button>View here</Button>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          <Heading size="md"> Customer dashboard</Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>View a summary of all your customers over the last month.</Text>
-        </CardBody>
-        <CardFooter>
-          <Button>View here</Button>
-        </CardFooter>
-      </Card>
-    </SimpleGrid>
-    <Box p="2">
-      <Button
-        leftIcon={<MdOutlineInventory size={"20px"} />}
-        color="teal"
-        variant="outline"
-        size={"sm"}
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(20rem, 1fr))"
       >
-        <Heading size="md">Inventory</Heading>
-      </Button>
-    </Box>
+        <Card onClick={callToast}>
+          <CardHeader>
+            <Heading size="md">
+              <Icon as={MdAdd} />
+              New Estimate
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+          {/* <CardFooter>
+          <Button leftIcon={<MdAdd size={"20px"} />}>Add New</Button>
+        </CardFooter> */}
+        </Card>
+        <Card onClick={callToast}>
+          <CardHeader>
+            <Heading size="md">
+              <Icon as={MdRemoveRedEye} />
+              Display
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+          {/* <CardFooter>
+          <Button leftIcon={<MdRemoveRedEye size={"20px"} />}> View </Button>
+        </CardFooter> */}
+        </Card>
+      </SimpleGrid>
+      <Box p="2">
+        <Tag size="lg" colorScheme="teal" borderRadius="full">
+          <TagLeftIcon boxSize="20px" as={MdOutlineInventory} />
+          <TagLabel>Inventory</TagLabel>
+        </Tag>
+      </Box>
 
-    <SimpleGrid
-      spacing={4}
-      templateColumns="repeat(auto-fill, minmax(20rem, 1fr))"
-    >
-      <Card>
-        <CardHeader>
-          <Heading size="md"> Customer dashboard</Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>View a summary of all your customers over the last month.</Text>
-        </CardBody>
-        <CardFooter>
-          <Button>View here</Button>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          <Heading size="md"> Customer dashboard</Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>View a summary of all your customers over the last month.</Text>
-        </CardBody>
-        <CardFooter>
-          <Button>View here</Button>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          <Heading size="md"> Customer dashboard</Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>View a summary of all your customers over the last month.</Text>
-        </CardBody>
-        <CardFooter>
-          <Button>View here</Button>
-        </CardFooter>
-      </Card>
-    </SimpleGrid>
-    <Box p="2">
-      <Button
-        leftIcon={<MdPeopleAlt size={"20px"} />}
-        color="teal"
-        variant="outline"
-        size={"sm"}
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(20rem, 1fr))"
       >
-        <Heading size="md">Customers</Heading>
-      </Button>
-    </Box>
-    <SimpleGrid
-      spacing={4}
-      templateColumns="repeat(auto-fill, minmax(20rem, 1fr))"
-    >
-      <Card>
-        <CardHeader>
-          <Heading size="md"> Customer dashboard</Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>View a summary of all your customers over the last month.</Text>
-        </CardBody>
-        <CardFooter>
-          <Button>View here</Button>
-        </CardFooter>
-      </Card>
-    </SimpleGrid>
-  </>
-);
+        <Card onClick={callToast}>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+        </Card>
+        <Card onClick={callToast}>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+        </Card>
+        <Card onClick={callToast}>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+        </Card>
+      </SimpleGrid>
+      <Box p="2">
+        <Tag size="lg" colorScheme="teal" borderRadius="full">
+          <TagLeftIcon boxSize="20px" as={MdPeopleAlt} />
+          <TagLabel>Customers</TagLabel>
+        </Tag>
+      </Box>
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(20rem, 1fr))"
+      >
+        <Card onClick={callToast}>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+        </Card>
+      </SimpleGrid>
+    </>
+  );
+};
